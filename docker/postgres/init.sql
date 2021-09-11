e60295983285565c2f7da407904359e38e85adca
max <sebastian.8295.8295@gmail.com>
2021-09-11 12:21:17 +0100

    database init

---
+++ docker/postgres/init.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS register (
+    guild_id INT NOT NULL,
+    message_id INT NOT NULL,
+    registry_name VARCHAR(64) NOT NULL,
+    embed_data JSONB,
+    role_id INT
+);
+
+COMMENT ON COLUMN register.guild_id IS 'The id of the guild';
+COMMENT ON COLUMN register.message_id IS 'The id of the message that the register is stored in';
+COMMENT ON COLUMN register.registry_name IS 'The registry name';
+COMMENT ON COLUMN register.embed_data IS 'Embed of all the registered users';
+COMMENT ON COLUMN register.role_id IS 'The id of the role that is used for announcements';
+
+CREATE TABLE IF NOT EXISTS schedule (
+    guild_id INT NOT NULL,
+    channel_id INT NOT NULL,
+    registry_name VARCHAR(64) NOT NULL,
+    scheduled_time TIMESTAMP WITH TIME ZONE NOT NULL,
+    role_id INT NOT NULL
+);
+
+COMMENT ON COLUMN schedule.guild_id IS 'The id of the guild';
+COMMENT ON COLUMN schedule.channel_id IS 'The id of the channel that the announcement is meant to be post in';
+COMMENT ON COLUMN schedule.registry_name IS 'The registry name';
+COMMENT ON COLUMN schedule.scheduled_time IS 'Timestamp of date and time of the due date to post the announcement';
+COMMENT ON COLUMN schedule.role_id IS 'The id of the role that is used for announcements';
--- src/db/db.ts
+++ src/db/db.ts
@@ -1,17 +1,15 @@
 import { Client } from 'pg';
 
 export default async () => {
-    let database = new Client()
-    // TODO Ciaran: Intialize tables if they do not yet exist
-    await database.connect()
+    let database = new Client({
+        user: process.env.POSTGRES_USER,
+        host: 'enrique',
+        database: 'enrique',
+        password: process.env.POSTGRES_PASSWORD,
+        port: 5432
+    })
 
-    // Intiate tables on startup if they do not exist,
-    //                   [These two are a key]
-    // Registrar: Holds, {Guild_ID, Message_ID, Registry_Name, Embed_Data[JSON], Role_ID}
-    //
-    // Events: Holds, {Guild_ID, Channel_ID, Time, Role_ID}
-    // Implement CRUD methods in CRUD folder for register, deregister and createRegistry in organization commands
-    // postgres node is a low abstraction library you will be writing queries manually
+    await database.connect()
 
     return database
 }
