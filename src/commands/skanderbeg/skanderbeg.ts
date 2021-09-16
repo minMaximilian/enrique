af32e03308c24f2940f6fc240ea914f811752c31
max <sebastian.8295.8295@gmail.com>
2021-09-16 19:18:14 +0100

    skanderbeg commands

--- src/commands/skanderbeg/skanderbeg.ts
+++ src/commands/skanderbeg/skanderbeg.ts
@@ -14,12 +14,38 @@ export default {
                 option.setName('game_id')
                 .setDescription('ID of the game it\'s at the end of the URL')
                 .setRequired(true))
+            )
+        .addSubcommand(subcommand => 
+            subcommand
+            .setName('area')
+            .setDescription('Gives you bragging rights by showing off the largest area')
+            .addStringOption(option => 
+                option.setName('game_id')
+                .setDescription('ID of the game it\'s at the end of the URL')
+                .setRequired(true))
+            )
+        .addSubcommand(subcommand => 
+            subcommand
+            .setName('missions')
+            .setDescription('Gives you information about most important missions')
+            .addStringOption(option => 
+                option.setName('game_id')
+                .setDescription('ID of the game it\'s at the end of the URL')
+                .setRequired(true))
             ),
     
 	async execute(interaction: CommandInteraction) {
         switch (interaction.options.getSubcommand()) {
             case 'devclicks':
                 await devclicks(interaction)
+                break;
+
+            case 'area':
+
+                break;
+
+            case 'missions':
+
                 break;
         }
 	},
--- src/commands/skanderbeg/subcommands/area.ts
+++ src/commands/skanderbeg/subcommands/area.ts
@@ -4,7 +4,7 @@ import { CommandInteraction, MessageEmbed } from "discord.js";
 
 export default async (interaction: CommandInteraction) => {
     const id = interaction.options.getString('game_id')
-    const res = await axios.get(`https://skanderbeg.pm/api.php?key=${process.env.SKANDERBEG_KEY}&scope=getCountryData&save=${id}&value=dev_clicks;player;dev_avg_cost;tag&format=json&playersOnly=true`)
+    const res = await axios.get(`https://skanderbeg.pm/api.php?key=${process.env.SKANDERBEG_KEY}&scope=getCountryData&save=${id}&value=dev_clicks;player;dev_avg_cost;tag;total_area;missions&format=json&playersOnly=true`)
     if (res.data === 'One of the savefiles is corrupted.') {
         interaction.reply({content: 'Please use a valid ID', ephemeral: true})
     } else {
@@ -15,7 +15,7 @@ export default async (interaction: CommandInteraction) => {
         console.log(l)
         l.sort((a, b) => Number(b.dev_clicks) - Number(a.dev_clicks))
     
-        const str = l.map((x, index) => `**${index + 1}: ${x.player}**\nCountry Tag: ${x.tag}\nDevelopment Clicks: ${x.dev_clicks}\nAverage Dev Cost: ${x.dev_avg_cost}`).join('\n\n')
+        const str = l.map((x, index) => `**${index + 1}: ${x.player}**\nCountry Tag: ${x.tag}\nLand Area: ${x.total_area}`).join('\n\n')
     
         const embed = new MessageEmbed()
                 .setColor("ORANGE")
