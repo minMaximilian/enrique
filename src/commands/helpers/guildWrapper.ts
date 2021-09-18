aed1ff132e7641bc7ed08a80dec549e388d755b4
max <sebastian.8295.8295@gmail.com>
2021-09-18 11:04:42 +0100

    got rid of bad dumb stupid wrapper

--- src/commands/helpers/guildWrapper.ts
+++
@@ -1,9 +0,0 @@
-import { CommandInteraction } from "discord.js";
-
-export default async (interaction: CommandInteraction, fn: Function, ...args: any[]) => {
-    if (interaction.channel?.type === 'GUILD_TEXT') {
-        await fn(interaction, ...args)
-    } else {
-        interaction.reply({content: 'This is a guild only command it doesn\'t work in DMs', ephemeral: true})
-    }
-}
--- src/commands/organization/signup.ts
+++ src/commands/organization/signup.ts
@@ -2,7 +2,6 @@ import { SlashCommandBuilder } from "@discordjs/builders";
 import { CommandInteraction, MessageEmbed, TextChannel } from "discord.js";
 import register from "../../db/crud/register";
 import removeRegistry from "../../db/crud/removeRegistry";
-import guildWrapper from "../helpers/guildWrapper";
 import registryEmbed from "./helpers/registryEmbed";
 
 export default {
@@ -21,29 +20,27 @@ export default {
             ),
 
 	async execute(interaction: CommandInteraction) {
-        guildWrapper(interaction, async (interaction: CommandInteraction) => {
-            const res = await register(interaction.guildId!, interaction.options.getString('registry_name')!, interaction.user.id, interaction.options.getString('role')!)
-            if (res) {
-                try {
-                    const chan = await interaction.guild?.channels.fetch(res[0].channel_id) as TextChannel
-                    const msg = await chan.messages.fetch(res[0].message_id)
-                    const list = res.map(x => `<@${x.uuid}>: ${x.role_text}`).join('\n')
-                    const embed = registryEmbed(interaction, list, msg.embeds[0].footer)
-                    
-                    if (!msg.deleted) {
-                        msg.edit({embeds: [embed]})     
-                    } else {
-                        throw new Error('Message Deleted');
-                    }
-        
-                    await interaction.reply({content: 'Succesfully registered', ephemeral: true})
-                } catch {
-                    removeRegistry(interaction.guildId!, interaction.options.getString('registry_name')!)
-                    await interaction.reply({content: `The registry board ${interaction.options.getString('registry_name')} doesn't exist`, ephemeral: true})
+        const res = await register(interaction.guildId!, interaction.options.getString('registry_name')!, interaction.user.id, interaction.options.getString('role')!)
+        if (res) {
+            try {
+                const chan = await interaction.guild?.channels.fetch(res[0].channel_id) as TextChannel
+                const msg = await chan.messages.fetch(res[0].message_id)
+                const list = res.map(x => `<@${x.uuid}>: ${x.role_text}`).join('\n')
+                const embed = registryEmbed(interaction, list, msg.embeds[0].footer)
+                
+                if (!msg.deleted) {
+                    msg.edit({embeds: [embed]})     
+                } else {
+                    throw new Error('Message Deleted');
                 }
-            } else {
+        
+                await interaction.reply({content: 'Succesfully registered', ephemeral: true})
+            } catch {
+                removeRegistry(interaction.guildId!, interaction.options.getString('registry_name')!)
                 await interaction.reply({content: `The registry board ${interaction.options.getString('registry_name')} doesn't exist`, ephemeral: true})
             }
-        })
+        } else {
+            await interaction.reply({content: `The registry board ${interaction.options.getString('registry_name')} doesn't exist`, ephemeral: true})
+        }
 	},
 };
